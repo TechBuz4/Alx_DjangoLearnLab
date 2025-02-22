a402981f2208da155009acfc130f75add709c91b
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-02-22 13:40:27 -0600

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book
+from .models import Library
 
 # Create your views here.
+def book_list_view(request):
+    """View to list all books with their authors."""
+    books = Book.objects.all()
+    return render(request, "relationship_app/book_list.html", {"books": books})
+
+class LibraryDetailView(DetailView):
+    """View to display a library's details and its books."""
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
