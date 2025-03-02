d55c9bfbd58570104c185b479afdc7f584ea0d79
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-03-01 20:12:25 -0600

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
+from .models import Book
+from .forms import BookForm
+
+@permission_required('app_name.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+
+# Create a new book - Requires `can_create` permission
+@permission_required('app_name.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
+
+
+# Edit a book - Requires `can_edit` permission
+@permission_required('app_name.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/book_form.html', {'form': form})
+
+
+# Delete a book - Requires `can_delete` permission
+@permission_required('app_name.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/book_confirm_delete.html', {'book': book})
+
