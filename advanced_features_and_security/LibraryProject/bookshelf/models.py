1e264d614dc22b3f6d4689f8629214111966eade
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-03-01 20:06:48 -0600

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
-# Author model (you only need this once)
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-# Book model (cleaned-up version)
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
@@ -17,48 +15,11 @@ class Book(models.Model):
 
     class Meta:
         permissions = [
-            ("can_add_book", "Can add a book"),
-            ("can_change_book", "Can change a book"),
-            ("can_delete_book", "Can delete a book"),
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
 
     def __str__(self):
         return self.title
-
-# Custom User Manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError(_("The Email field must be set"))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-
-        return self.create_user(email, password, **extra_fields)
-
-# Custom User Model
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
-
-    username = None  # This disables the default `username` field, using email instead.
-
-    USERNAME_FIELD = 'email'   
-    REQUIRED_FIELDS = []  # You can add 'date_of_birth' here if you want to make it mandatory
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
