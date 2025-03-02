d67d85d9d79e594136d375ad19eac73d72273121
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-03-01 22:30:06 -0600

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,12 +1,6 @@
 from django import forms
 from bookshelf.models import Book
 
-class ExampleForm(forms.Form):
-    query = forms.CharField(max_length=100, required=True)
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_date']
-        widgets = {
-            'publication_date': forms.DateInput(attrs={'type': 'date'}),
-        }
+class BookSearchForm(forms.Form):
+    search_query = forms.CharField(max_length=100, required=False)
+
