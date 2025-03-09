8024df35f1a8fb05f39e4778d8ae5deb897a5dd1
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-03-08 20:11:08 -0600

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -52,6 +52,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 ROOT_URLCONF = 'api_project.urls'
 
