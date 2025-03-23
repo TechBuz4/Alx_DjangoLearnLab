0dcf0832e3f60b68bc08738cb94c95d9b4e9ca8f
Busayo Ososanwo <ososanwo2020@gmail.com>
2025-03-23 09:39:42 -0500

    added url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import register_view, login_view, logout_view, profile_view,profile_update_view
 from django.contrib.auth import views as auth_views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path("register/", register_view, name="register"),
@@ -8,5 +9,9 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("profile/", profile_view, name="profile"),
     path("profile/update/", profile_update_view, name="profile_update"),
-
+    path('', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
