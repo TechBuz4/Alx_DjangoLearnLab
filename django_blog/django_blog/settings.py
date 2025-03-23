7a5892faa6cc99538b1ced05ffdb76c0e75cff5c
Busayo Ososanwo <ososanwo2020@gmail.com>
2025-03-23 08:44:10 -0500

    added post

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'busayo_db',
+        'USER': 'busayo',
+        'PASSWORD': 'boob',
+        'HOST': 'localhost', 
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
